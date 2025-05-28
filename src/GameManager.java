1072bc25467ed97e837dd3f26cc188d1aa6d3352
Sam <samanthaverrazano@gmail.com>
2025-05-27 23:05:18 -0400

    Update

---
+++ src/GameManager.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class GameManager {
+
+    UI ui = new UI(this);
+    
+    public static void main(String[] args) {
+        new GameManager();
+    }
+
+    public GameManager() {
+    }
+}
